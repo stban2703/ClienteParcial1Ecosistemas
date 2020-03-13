6c393382b8cb1c79472e3cea6fdf10cd8978bf6d
stban2703 <s.t.ban1401@gmail.com>
2020-03-13 15:27:49 -0500

    Envia y crea recordatorios

--- app/src/main/java/com/example/clienteparcial1ecosistemas/ComunicacionTCP.java
+++ app/src/main/java/com/example/clienteparcial1ecosistemas/ComunicacionTCP.java
@@ -2,12 +2,84 @@ package com.example.clienteparcial1ecosistemas;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class ComunicacionTCP {
+public class ComunicacionTCP extends Thread{
 
     private Socket socket;
     private BufferedReader reader;
     private BufferedWriter writer;
+    private MainActivity app;
 
+    public ComunicacionTCP(MainActivity app) {
+        this.app = app;
+
+    }
+
+    //Hilo de recepción
+    public void run() {
+        try {
+            this.socket = new Socket("10.0.2.2", 5000);
+
+            //Reader
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            this.reader = new BufferedReader(isr);
+
+            //Writer
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            this.writer = new BufferedWriter(osw);
+
+            while (true) {
+                recibirMensaje();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //Solicitar conexion
+    public void solicitarConexion(/*String ip*/) {
+        //this.ip = ip;
+        this.start();
+    }
+
+    //Mandar un mensaje
+    public void mandarMensaje(String mensaje) {
+        new Thread(
+                () -> {
+                    try {
+                        writer.write(mensaje + "\n");
+                        writer.flush();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
+    }
+
+    //Recibir mensaje
+    public void recibirMensaje() throws IOException {
+        String line = reader.readLine();
+
+
+    }
+
+    public void cerrarConexion() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/clienteparcial1ecosistemas/MainActivity.java
+++ app/src/main/java/com/example/clienteparcial1ecosistemas/MainActivity.java
@@ -3,12 +3,68 @@ package com.example.clienteparcial1ecosistemas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variable comunicacion
+    private ComunicacionTCP comm;
+
+    //Variable para componenetes de la interfaz
+    private EditText posxT;
+    private EditText posyT;
+    private EditText recorT;
+    private RadioButton leveBtn;
+    private RadioButton mediaBtn;
+    private RadioButton altaBtn;
+    private Button vistaBtn;
+    private Button confirmarBtn;
+
+    //Variables para guardar mensajes
+    private String posX;
+    private String posY;
+    private String recordatorio;
+    private String importancia;
+    private String mensajeRecordatorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        comm = new ComunicacionTCP(this);
+        comm.solicitarConexion();
+
+        posxT = findViewById(R.id.posxT);
+        posyT = findViewById(R.id.posyT);
+        recorT = findViewById(R.id.recorT);
+        leveBtn = findViewById(R.id.leveBtn);
+        mediaBtn = findViewById(R.id.mediaBtn);
+        altaBtn = findViewById(R.id.altaBtn);
+        vistaBtn = findViewById(R.id.vistaBtn);
+        confirmarBtn = findViewById(R.id.confirmarBtn);
+
+        confirmarBtn.setOnClickListener(
+                (v) -> {
+                    posX = posxT.getText().toString();
+                    posY = posyT.getText().toString();
+                    recordatorio = recorT.getText().toString();
+
+                    mensajeRecordatorio = posX +"," + posY +"," + recordatorio;
+
+                    if(!posX.equals("") && !posY.equals("") && !recordatorio.equals("")) {
+                        comm.mandarMensaje(mensajeRecordatorio);
+
+                    } else {
+                        runOnUiThread(
+                                () -> {
+                                    Toast.makeText(this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+                                }
+                        );
+                    }
+                }
+        );
     }
 }
