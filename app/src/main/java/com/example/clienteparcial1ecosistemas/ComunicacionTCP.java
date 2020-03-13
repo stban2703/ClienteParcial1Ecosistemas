29ed6406ec48570795ea255e163db0cb5d7e5d58
stban2703 <s.t.ban1401@gmail.com>
2020-03-13 13:49:51 -0500

    Inicio de variables

--- app/src/main/java/com/example/clienteparcial1ecosistemas/ComunicacionTCP.java
+++ app/src/main/java/com/example/clienteparcial1ecosistemas/ComunicacionTCP.java
@@ -1,4 +1,13 @@
 package com.example.clienteparcial1ecosistemas;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+
 public class ComunicacionTCP {
+
+    private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
 }
